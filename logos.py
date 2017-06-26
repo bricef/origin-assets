aa9e26823b4be666c36ad611f05b4524b4f1b29d
Brice Fernandes <brice@sigmaforge.com>
2017-06-26 13:35:45 +0100

    Use Pillow instead of PIL and improve resolution of output

--- logos.py
+++ logos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
-import Image
-import ImageDraw
+from PIL import Image
+from PIL import ImageDraw
 import numpy as np
 
 CREAM="#e9f3fe"
@@ -15,12 +15,12 @@ def ring(im, origin, radius, thickness=2, color=FOREGROUND):
   outer_ring = tuple(np.array(outer_box)+np.array([-thickness,-thickness,thickness,thickness]))
   inner_ring = tuple(
     np.array(outer_ring)
-    + 
-    np.array([thickness*2,thickness*2,-thickness*2,-thickness*2])) 
+    +
+    np.array([thickness*2,thickness*2,-thickness*2,-thickness*2]))
 
   cream_all = Image.new("RGBA", im.size, color)
   mask = Image.new("1", im.size, 1)
-  
+
   draw = ImageDraw.Draw(mask)
   draw.ellipse(outer_ring, fill=0 )
   draw.ellipse(inner_ring, fill=1 )
@@ -40,12 +40,12 @@ def dot(im, xy, radius=10, border=0, color=FOREGROUND, bordercolor=BACKGROUND):
 
 def circlet(im, origin, radius, thickness=5, num=13, dotrad=15, border=5, fore=FOREGROUND, back=BACKGROUND):
 
-  im = ring(im,origin, radius, thickness=3, color=fore)
+  im = ring(im,origin, radius, thickness=thickness, color=fore)
 
   angles = [((2*np.pi)/num)*x for x in range(num)]
   coords = [(np.sin(theta)*radius, np.cos(theta)*radius) for theta in angles]
   points = [(x+origin[0], y+origin[1]) for x,y in coords]
-    
+
   for point in points:
     im = dot(im, point, radius=3*thickness,border=thickness, color=fore, bordercolor=back)
   return im
@@ -64,7 +64,7 @@ def scaledring(im, origin, radius, scale=0.4, thickness=5, color=FOREGROUND):
     np.array([0,-thickness*scale,0,-thickness*scale ])
     )
   mask = Image.new("1", im.size, 1)
-  
+
   draw = ImageDraw.Draw(mask)
   draw.ellipse(outer_ring, fill=0 )
   draw.ellipse(inner_ring, fill=1 )
@@ -73,7 +73,7 @@ def scaledring(im, origin, radius, scale=0.4, thickness=5, color=FOREGROUND):
 
 
 if __name__ == "__main__":
-  im = Image.new("RGBA", (600,400), BACKGROUND)
+  im = Image.new("RGBA", (1200,800), BACKGROUND)
 #  im = scaledring(im, (300,200), 150, scale=1.5, thickness=2, color = FOREGROUND)
-  im = circlet(im, (300,200), 150, fore = FOREGROUND, back = BACKGROUND)
+  im = circlet(im, (600,400), 300, thickness=10, dotrad=30, border=10, fore = FOREGROUND, back = BACKGROUND)
   im.save("try.png")
